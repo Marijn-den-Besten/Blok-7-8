d227927096e6c13d881caaa7ff7c72379a472f2b
Marijn <marijndbesten@gmail.com>
2021-02-11 16:02:52 +0100

    Add image to page

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-console.log('hello world!');
+var names = ['Bert', 'Chantal', 'Defvin', 'Freek', 'Gerda', 'Gert', 'Hanneke', 'Hans', 'Irma', 'Karen', 'Kees', 'Lidia'];
 
-var names = ['bert', 'chantal', 'freek'];
+var number = Math.floor(Math.random() * 12);
+var name = names[number]
 
-names.forEach(showNames)
+console.log('Correct number: ' + number);
+console.log('Correct name: ' + name);
 
-function showNames(item) {
-    document.getElementById('namecol').innerHTML += 'Name:' + '<br>';
-    document.getElementById('itemcol').innerHTML += item  + '<br>';
+showImage(name);
+createAnswers(name);
+
+function showImage(image) {
+    console.log('In function showImage');
 
     var img = document.createElement('img');
-    img.src = '/Jaar%202/Blok%207%20+%208/Images/'+item+'.jfif';
-    img.width = 100;
-    img.height = 100;
-    document.getElementById('images').appendChild(img);
+
+    img.src = 'Images/' + image + '.jfif';
+    img.width = 500;
+    img.height = 500;
+
+    document.getElementById('image').appendChild(img);
+}
+
+function createAnswers(correctName) {
+    console.log('In function createAnswers');
+
+    var answers = [correctName];
+
+    while (answers.length !== 3) {
+        var randomName = names[Math.floor(Math.random() * 12)];
+        if (randomName !== correctName) {
+            if (randomName !== answers[1]) {
+                answers.push(randomName);
+            }
+        }
+    }
+
+    for (var i = 0; i < answers.length; i++) {
+        var coldiv = document.createElement('div');
+        coldiv.className = 'col-4';
+
+        document.getElementById('answersrow').appendChild(coldiv);
+
+        var button = document.createElement('button');
+        button.id = answers[i];
+        button.className = 'btn-primary btn-block';
+        button.innerHTML = answers[i];
+        button.onclick = function () {
+            registerAnswer(this.id);
+        };
+
+        coldiv.appendChild(button);
+    }
+}
+
+function registerAnswer(answer) {
+    console.log('In function registerAnswer');
+    console.log(answer);
 }
